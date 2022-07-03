4527f80f55cda709bbd86f9b9a3a6bb7a3216f6b
videsh pundir <videsh5655@gmail.com>
2022-07-04 03:49:24 +0530

    my project

---
+++ src/app/product.ts
@@ -0,0 +1,11 @@
+// ng generate class product
+export class Product {
+
+    id: String | any;
+    name: string |any;
+    brand: string |any;
+    madein: string |any;
+    price:number |any;
+    active: boolean =false;
+
+}
